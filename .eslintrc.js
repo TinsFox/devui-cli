90f4b3e012da56eae5f9ec9c92cf726e19e1b703
TinsFox <1414849373@qq.com>
2022-05-08 22:17:04 +0800

    chore: remove fabric package

--- .eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: [require.resolve("@devui/fabric/dist/eslint")],
-};
--- packages/devui-fabric/src/index.ts
+++
@@ -1,5 +0,0 @@
-const strictEslint = require('./eslint');
-
-module.exports = {
-  strictEslint
-};
